ba7c3f9438967771b26339c91f959ffcd2fb4d89
OgyreZ1 <pe4enbko1029@gmail.com>
2019-07-13 01:35:24 +0300

    Create domain model for books and authors

---
+++ BiblioRead/Models/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BiblioRead.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+
+        public Author()
+        {
+            Books = new Collection<Book>();
+        }
+    }
+}
