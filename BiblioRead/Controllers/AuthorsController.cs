4e28b214f8aee6d7e09afc6c1fdc7b1709a4c56d
OgyreZ1 <pe4enbko1029@gmail.com>
2019-07-15 12:59:34 +0300

    Authors API

---
+++ BiblioRead/Controllers/AuthorsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BiblioRead.Controllers.Resources;
+using BiblioRead.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BiblioRead.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public AuthorsController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AuthorResource>> GetAuthors()
+        {
+            var authors = await context.Authors.Include(b => b.Books).ToListAsync();
+
+            return mapper.Map<List<Author>, List<AuthorResource>>(authors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AuthorResource> GetAuthor(int id)
+        {
+            var author = await context.Authors.Include(b => b.Books).SingleOrDefaultAsync(b => b.Id == id);
+
+            return mapper.Map<Author, AuthorResource>(author);
+        }
+
+
+    }
+}
